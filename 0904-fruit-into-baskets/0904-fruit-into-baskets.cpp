1ed6d8f379c57e873a6c4b6f258e0df8407c528c
psycho4082 <107204831+psycho4082@users.noreply.github.com>
2023-02-08 19:01:01 +0530

    Time: 203 ms (41.20%), Space: 75.3 MB (6.69%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -1,32 +1,25 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-        // We use a hash map 'basket' to store the number of each type of fruit.
-        unordered_map<int, int> basket;
-        int left = 0, maxPicked = 0;
-        
-        // Add fruit from the right index (right) of the window.
-        for (int right = 0; right < fruits.size(); ++right) {
-            basket[fruits[right]]++;
+        int left = 0 ;
+        int maxi = 0 ;
+       map<int,int>mp;
+        for(int i =0; i<fruits.size();i++){
             
-            // If the current window has more than 2 types of fruit,
-            // we remove fruit from the left index (left) of the window,
-            // until the window has only 2 types of fruit.
-            while (basket.size() > 2) {
-                basket[fruits[left]]--;
-                if (basket[fruits[left]] == 0)
-             {
-                    basket.erase(fruits[left]);
+            mp[fruits[i]]++;
+                while(mp.size()>2){
+                    
+                    mp[fruits[left]]--;
+                    if(mp[fruits[left]] == 0)
+                    {
+                        mp.erase(fruits[left]);
+                    }
+                    left++;
                 }
-                left++;
-         //       }
-            }
+            maxi = max(maxi,i-left+1); 
             
-            // Update maxPicked.
-            maxPicked = max(maxPicked, right - left + 1);
         }
+        return maxi ;
         
-        // Return maxPicked as the maximum number of fruits we can collect.
-        return maxPicked;
     }
 };
